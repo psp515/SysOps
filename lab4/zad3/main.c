5605ddf64d640bad84f59eebab68caceb0429201
Łukasz Kolber <69080157+psp515@users.noreply.github.com>
2023-03-24 15:21:33 +0100

    Working version

--- lab4/zad3/main.c
+++ lab4/zad3/main.c
@@ -5,14 +5,15 @@
 #include <sys/types.h>
 #include <time.h>
 
+#define MAX_LEN 10
+
 static int requests = 0;
-static int is_working = 1;
 static int mode = 0;
 
 void print_numbers()
 {
     for (int i = 1; i <= 100; i++) 
-        printf("%d\n", i);
+        printf("C: %d\n", i);
     
 }
 
@@ -20,48 +21,40 @@ void print_time()
 {
     time_t t;
     time(&t);
-    printf("Current time: %s\n", ctime(&t));
+    printf("C: Current time: %s\n", ctime(&t));
 }
 
 void off()
 {
-    printf("Finished\n");
+    printf("C: Finished\n");
     exit(0);
 }
 
 void handler(int sig, siginfo_t* info, void* context)
 {
-    if(sig != SIGUSR1)
-        return;
 
-    printf("Signal: %d\n", sig);
-    printf("From Pid: %d\n", info->si_pid);
+    printf("C: Signal: %d\n", sig);
+    printf("C: From Pid: %d\n", info->si_pid);
     int new_mode = info->si_value.sival_int;
-    printf("Mode: %d\n", new_mode);
+    printf("C: Mode: %d\n", new_mode);
 
     requests += 1;
-
     mode = new_mode;
 
-    if(mode == 1)
-        print_numbers();
-    else if (mode == 2)
-        print_time();
-    else if(mode == 3)
-        printf("Received data: %d", requests);
-    else if(mode == 5)
-        off();
-
-    kill(info->si_pid, SIGUSR1);
+    printf("C: Send Confirmation.\n");
+    int resp = kill(info->si_pid, SIGUSR1);
+    if(resp == -1)
+    {
+        perror("C: Catcher to sender error.");
+        exit(1);
+    }
 }
 
 int main()
 {
     pid_t pid = getpid();
-    printf("Catcher started.\n");
-    printf("Pid: %d\n", pid);
-
-    execl("./sender", "sender", pid, NULL);
+    printf("-------------Catcher started.-------------\n");
+    printf("C: Pid: %d\n", pid);
 
     struct sigaction act;
     act.sa_flags = SA_SIGINFO;
@@ -69,16 +62,50 @@ int main()
     sigemptyset(&act.sa_mask);
 
     if (sigaction(SIGUSR1, &act, NULL) < 0) {
-        perror("Sigaction error.\n");
+        perror("C: Sigaction error.\n");
         exit(1);
     }
 
+    pid_t scnd_pid = fork();
 
-    while(1)
+    if(scnd_pid == 0)
     {
-        if(mode == 4)
-            print_time();
-        
-        sleep(1);
+        char str_pid[MAX_LEN];
+        sprintf(str_pid,"%d", pid);
+        int resp = execl("./sender", "sender", str_pid, "2", "2", "5", NULL);
+
+        if(resp == -1)
+        {
+            perror("C: Sender not started.");
+            exit(1);
+        }
     }
+
+    while(mode != 5)
+    {
+        switch(mode)
+        {
+            case 1:
+                print_numbers();
+                mode = 0;
+                break;
+            case 2:
+                print_time();
+                mode = 0;
+                break;
+            case 3:
+                printf("C: Received data: %d\n", requests);
+                mode = 0;
+                break;
+            case 4:
+                print_time();
+                sleep(1);
+                break;
+            default:
+                continue;
+        }
+
+    }
+
+    printf("Catcher finished.\n");
 }
--- lab4/zad3/sender.c
+++ lab4/zad3/sender.c
@@ -3,10 +3,6 @@
 #include <stdlib.h>
 #include <unistd.h>
 #include <sys/types.h>
-#include <sys/wait.h>
-
-int sigqueue(pid_t pid, int sig, const union sigval value);
-
 
 void send_signal(pid_t pid, int signal_num, int value) {
     union sigval sigval_value;
@@ -14,67 +10,56 @@ void send_signal(pid_t pid, int signal_num, int value) {
     sigqueue(pid, signal_num, sigval_value);
 }
 
-void wait_for_signal(int signal_num) {
-    sigset_t sigset;
-    sigemptyset(&sigset);
-    sigaddset(&sigset, signal_num);
-    while(1) {
-        sigsuspend(&sigset);
-        return;
-    }
+void handler(int sig, siginfo_t* info, void* context)
+{
+    printf("S: Confirmation received.\n");
 }
 
-int main(int n, char *args[]) {
+int main(int n, char **args) {
      if(n < 2) {
-        printf("Usage: ./sender <catcher_pid>");
+        printf("Usage: ./sender <catcher_pid>\n");
         exit(1);
     }
 
     pid_t catcher_pid = atoi(args[1]);
-    /*int time_span = min(2, atoi(args[2]));
+
+    printf("------------- Sender started.--------------\n");
+    printf("S: Pid: %d\n", getpid());
+    printf("S: Catcher pid: %d\n", catcher_pid);
+    printf("S: Enter numbers in the range 1-5:\n");
 
     int modes[n-2];
-
     for(int i = 2; i < n; i++) {
         modes[i-2] = atoi(args[i]);
 
         if(modes[i-2] < 1 || modes[i-2]> 5)
         {
-            perror("Data Error. Ending program.");
+            perror("Data Error. Ending program.\n");
             send_signal(catcher_pid, SIGUSR1, 5);
-            wait_for_signal(SIGUSR1);
             exit(1);
         }
     }
 
+    struct sigaction act;
+    act.sa_flags = SA_SIGINFO;
+    act.sa_handler = handler;
+    if (sigaction(SIGUSR1, &act, NULL) < 0) {
+        perror("S: Sigaction error.\n");
+        exit(1);
+    }
+
     for(int i = 0; i < n-2; i++) {
         int mode = modes[i];
-        printf("Sending mode %d\n", mode);
+        printf("S: Sending mode %d\n", mode);
         send_signal(catcher_pid, SIGUSR1, mode);
-        wait_for_signal(SIGUSR1);
-    } */
-
-
-    int mode;
-
-    printf("Enter numbers in the range 1-5:\n");
-
-    while (scanf("%d", &mode) == 1) {
-        if (mode >= 1 && mode <= 5)
-        {
-            printf("Sending mode %d\n", mode);
-            send_signal(catcher_pid, SIGUSR1, mode);
-            wait_for_signal(SIGUSR1);
-        }
-        else 
-            printf("Invalid number. Please enter a number in the range 1-5.\n");
-
-        if(mode == 5)
-            break;
+        printf("S: Signal send %d\n", mode);
+        sigsuspend(&act.sa_mask);
+        printf("S: Signal received %d\n", mode);
     }
 
 
-    printf("All modes sent successfully!\n");
+    printf("S: All modes sent successfully!\n");
 
     return 0;
 }
+
